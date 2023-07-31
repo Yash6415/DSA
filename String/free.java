581b13285fbc52ad9c9c4a2ad1be8591dc08585d
Yash Sojitra <yashsojitra065@gmail.com>
2023-07-31 17:16:13 +0530

    done

--- String/free.java
+++ String/free.java
@@ -1,21 +1,31 @@
 package String;
-
 import java.util.*;
-
 public class free {
     public static void main(String[] args) {
-        String s = "naman is a naman";
-        int[] arr = new int[128];
-
+        String s = "msm sasg";
+        char[] arr = s.toCharArray();
+        int left = 0;
+        int right = 0;
         for(int i=0; i<s.length(); i++){
-            int ascii = s.charAt(i);
-            if(arr[ascii] == 0){
-                arr[ascii] += 1;
-            } else if (arr[ascii] == 1) {
-                arr[ascii] += 1;
-                char c = s.charAt(i);
-                System.out.println(c);
+            if(arr[i] == ' '){
+                right = i - 1;
+                helper(arr,left,right);
+                left = i + 1;
             }
         }
+        right = arr.length - 1;
+        helper(arr,left,right);
+    }
+    private static void helper(char[] arr,int left, int rigth){
+        while (left < rigth){
+            if(arr[left] != arr[rigth]){
+                System.out.print(false);
+                return;
+            }else {
+                left++;
+                rigth--;
+            }
+        }
+        System.out.println(true);
     }
 }
--- String/sortstring.java
+++ String/sortstring.java
@@ -5,8 +5,8 @@ public class sortstring {
         String s = "yash";
         char[] c = s.toCharArray();
 
-        for(int i=0; i<c.length-1; i++){
-            for(int j=i+1; j<c.length; j++){
+        for(int i=0; i<c.length; i++){
+            for(int j=i+1; j<c.length-1; j++){
                 if(c[i] > c[j]){
                     char temp = c[i];
                     c[i] = c[j];
