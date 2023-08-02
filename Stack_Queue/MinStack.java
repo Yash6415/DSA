a33324bfe4be36760b664eb9a13c01fd1a2b661b
Yash Sojitra <yashsojitra065@gmail.com>
2023-08-02 12:50:36 +0530

    done

---
+++ Stack_Queue/MinStack.java
@@ -0,0 +1,12 @@
+package Stack_Queue;
+
+import java.util.Stack;
+
+public class MinStack {
+    static Stack<Integer> stack = new Stack<>();
+    static Stack<Integer> min = new Stack<>();
+    public static void main(String[] args) {
+
+    }
+    
+}
