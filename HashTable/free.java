41e90d8ffe4e1329a342e69721f33d5d00c43e0f
Yash Sojitra <yashsojitra065@gmail.com>
2023-08-12 18:39:36 +0530

    12-08-2023

--- HashTable/free.java
+++ HashTable/free.java
@@ -1,31 +1,8 @@
 package HashTable;
-
 import java.util.*;
-
 public class free {
     public static void main(String[] args){
-        String s = "leetcode";
-        String t = "coats";
-        HashMap<Character,Integer> map1 = new HashMap<>();
-        HashMap<Character,Integer> map2 = new HashMap<>();
 
-        for(char c:s.toCharArray()){map1.put(c,map1.getOrDefault(c,0)+1);}
-        for(char c:t.toCharArray()){map2.put(c,map2.getOrDefault(c,0)+1);}
 
-        int count = 0;
-        for(char c:map1.keySet()){
-            if(map2.containsKey(c)) {
-                count+=Math.abs(map2.get(c) - map1.get(c));
-            }
-            else{
-                count+=map1.get(c);
-            }
-        }
-        for(char c:map2.keySet()){
-            if(!map1.containsKey(c)){
-                count+=map2.get(c);
-            }
-        }
-        System.out.println(count);
     }
 }
--- HashTable/sumExit.java
+++ HashTable/sumExit.java
@@ -8,24 +8,24 @@ public class sumExit {
         int sum = 10;
         HashSet<Integer> set = new HashSet<>();
 
-        for(int i=0; i< arr.length; i++) {
-            if (!set.contains(arr[i]) && set.contains(sum - arr[i])) {
-                System.out.println(true);
-                return;
-            } else {
-                set.add(arr[i]);
-            }
-        }
-//        HashMap<Integer,Integer> map = new HashMap<>();
-//
-//        for(int i=0; i< arr.length; i++){
-//            int key = sum - arr[i];
-//            if(map.containsKey(key)){
-//                System.out.print(true);
+//        for(int i=0; i< arr.length; i++) {
+//            if (!set.contains(arr[i]) && set.contains(sum - arr[i])) {
+//                System.out.println(true);
 //                return;
+//            } else {
+//                set.add(arr[i]);
 //            }
-//            map.put(arr[i],i);
 //        }
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        for(int i=0; i< arr.length; i++){
+            int key = sum - arr[i];
+            if(map.containsKey(key)){
+                System.out.print(true);
+                return;
+            }
+            map.put(arr[i],i);
+        }
         System.out.print(false);
     }
 }
