11e3e68e5bf95f52e59f3fb6a6c5a772b387c746
Yash Sojitra <yashsojitra065@gmail.com>
2023-08-18 11:05:05 +0530

    18-08-2023

--- LeetCode/LeetCode152.java
+++ LeetCode/LeetCode152.java
@@ -1,9 +1,9 @@
 package LeetCode;
-import java.util.*;
-
-public class free {
+//18-08-2023
+//152.Maximum Product Subarray.
+public class LeetCode152 {
     public static void main(String[] args) {
-        int arr[] = {-1, -2, -3};
+        int arr[] = {2,3,-2,4};
         int n = arr.length;
 
         int pre = 1;
@@ -11,10 +11,10 @@ public class free {
         int ans = Integer.MIN_VALUE;
 
         for (int i = 0; i < n; i++) {
-            if (pre == 0) {
+            if (pre == 0){
                 pre = 1;
             }
-            if (suff == 0) {
+            if (suff == 0){
                 suff = 1;
             }
             pre *= arr[i];
