d86672655440f32b6790a9a957974c853df441ee
Yash Sojitra <yashsojitra065@gmail.com>
2023-08-20 09:29:02 +0530

    20-08-2023

--- LeetCode/LeetCode1550.java
+++ LeetCode/LeetCode1550.java
@@ -6,8 +6,8 @@ public class LeetCode1550 {
         int[] arr = {1,2,34,3,4,5,7,23,12};
         int count = 0;
 
-        for (int num : arr) {
-            if (num % 2 == 1) {
+        for (int i : arr) {
+            if (i % 2 == 1) {
                 count++;
                 if (count == 3) {
                     System.out.println(true);
