3a5dd00d1a090708d01393388b6c63838675dfac
Yash Sojitra <yashsojitra065@gmail.com>
2023-08-21 11:42:03 +0530

    21-08-2023

--- LeetCode/LeetCode1004.java
+++ LeetCode/LeetCode1004.java
@@ -1,5 +1,6 @@
 package LeetCode;
-
+//21-08-2023
+//1004.Max Consecutive Ones III.
 public class LeetCode1004 {
     public static void main(String[] args) {
         int[] arr = {1,1,1,0,0,0,1,1,1,1,0};
