5121cb2cc6edaee6115232cac0d00e359444b212
Yash Sojitra <yashsojitra065@gmail.com>
2023-08-26 16:46:53 +0530

    26-08-2023

--- Math/LeetCode728.java
+++ Math/LeetCode728.java
@@ -1,19 +1,29 @@
 package Math;
-
-import java.util.ArrayList;
-import java.util.List;
-
+import java.util.*;
+//26-08-2023
+//728.Self Dividing Numbers.
 public class LeetCode728 {
     public static void main(String[] args) {
         int left = 1;
         int right = 22;
-        List<Integer> list = new ArrayList<>();
+        List<Integer> ans = new ArrayList();
 
-        for(int i=left; i<=right; i++){
-            if(i % 2 == 0){
-                list.add(i);
+        for (int i=left; i<=right; i++) {
+            if (helper(i)) {
+                ans.add(i);
             }
         }
-        System.out.println(list);
+        System.out.println(ans);
+    }
+    public static boolean helper(int n) {
+        int num = n;
+        while (num > 0) {
+            int digit = num % 10;
+            if (digit == 0 || n % digit != 0) {
+                return false;
+            }
+            num /= 10;
+        }
+        return true;
     }
 }
