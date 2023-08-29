813080f70be9253366bd27f00f083c34cc849d6a
Yash Sojitra <yashsojitra065@gmail.com>
2023-08-29 17:32:18 +0530

    29-08-2023

--- Arrays/free.java
+++ Arrays/free.java
@@ -1,7 +1,11 @@
 package Arrays;
 import java.util.Arrays;
-public class free {
-    public static void main(String[] args) {
+public final class free {
+    private static final int STATUS = getStatus();
 
+    private static int getStatus() {
+        System.out.println("Hello World!!");
+        System.exit(0);
+        return 0;
     }
 }
