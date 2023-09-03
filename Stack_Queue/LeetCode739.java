0d41c380711367255685854ead475753a335e43a
Yash Sojitra <yashsojitra065@gmail.com>
2023-09-03 10:19:50 +0530

    03-09-2023

--- Stack_Queue/LeetCode739.java
+++ Stack_Queue/LeetCode739.java
@@ -1,29 +1,24 @@
 package Stack_Queue;
-
-import java.util.Arrays;
-//02-08-2023
+import java.util.*;
+//24-07-2023
 //739.Daily Temperatures.
 public class LeetCode739 {
     public static void main(String[] args) {
         int[] arr = {73,74,75,71,69,72,76,73};
         int[] ans = new int[arr.length];
+        Deque<Integer> deque = new ArrayDeque<>();
 
         for (int i = 0; i < arr.length; i++) {
-            int count = 0;
-            boolean found = false;
-            for (int j = i + 1; j < arr.length; j++) {
-                if (arr[i] < arr[j]) {
-                    ans[i] = ++count;
-                    found = true;
-                    break;
-                } else {
-                    count++;
-                }
+            int n = arr[i];
+            while (!deque.isEmpty() && arr[deque.peek()] < n) {
+                int j = deque.pop();
+                ans[j] = i - j;
             }
-            if (!found) {
-                ans[i] = 0;
-            }
+            deque.push(i);
         }
-        System.out.println(Arrays.toString(ans));
+
+        for(int i : ans){
+            System.out.print(i + " ");
+        }
     }
 }
