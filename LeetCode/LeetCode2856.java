e5cfd56751aa04983f1ce2ba8fdf715add10f564
Yash Sojitra <yashsojitra065@gmail.com>
2023-10-09 16:37:37 +0530

    09-10-2023

--- LeetCode/LeetCode2856.java
+++ LeetCode/LeetCode2856.java
@@ -1,22 +1,29 @@
 package LeetCode;
 import java.util.*;
-
+//07-10-2023
+//2856.Minimum Array Length After Pair Removals.
 public class LeetCode2856 {
     public static void main(String[] args) {
         List<Integer> list = new ArrayList<>();
-        list.add(1);
         list.add(2);
-//        list.add(2);
-//        list.add(9////);
-        int ans = list.size();
-        Collections.sort(list);
+        list.add(3);
+        list.add(4);
+//        list.add(9);
 
-        int max = list.size() / 2;
-        for (int i=0; i<list.size(); i++) {
-            if (i < max || i >= list.size() - max) {
-                ans--;
+        int i = 0;
+        int j = list.size()/2;
+
+        int ans = 0;
+        while(j < list.size()){
+            if(!list.get(i).equals(list.get(j))){
+                ans += 2;
             }
+            i++;
+            j++;
         }
-        System.out.println(ans);
+        if(list.size() - ans >= 0){
+            System.out.println(list.size() - ans);
+        }
+        System.out.println(1);
     }
 }
